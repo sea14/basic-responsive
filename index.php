402bda1fb67050a0321f7b0db401b8e95784edd5
Sharon <seaustin@live.unc.edu>
2014-07-09 22:12:27 -0400

    index skeleton

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<div id="content">
+
+</div><!--close content-->
+
+</div> <!--close container-->
+
+	<div id="primary" class="widget-area">
+
+	</div> <!-- close primary & widget area-->
+
+	<div id="secondary" class="widget-area">
+
+	</div> <!--close secondary-->
+
+<?php get_footer(); ?>
